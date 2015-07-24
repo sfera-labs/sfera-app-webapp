1842bfaf685aea5ec901afaf1caba9546d76495f
Giampiero <giampiero7@users.noreply.github.com>
2015-07-24 18:42:18 +0200

    cams

--- src/main/java/cc/sferalabs/sfera/apps/webapp/WebApp.java
+++ src/main/java/cc/sferalabs/sfera/apps/webapp/WebApp.java
@@ -3,10 +3,13 @@ package cc.sferalabs.sfera.apps.webapp;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import com.google.common.eventbus.Subscribe;
+
 import cc.sferalabs.sfera.apps.Application;
 import cc.sferalabs.sfera.core.Configuration;
 import cc.sferalabs.sfera.http.HttpServer;
 import cc.sferalabs.sfera.http.HttpServerException;
+import cc.sferalabs.sfera.http.api.HttpEvent;
 
 public class WebApp extends Application {
 
@@ -39,6 +42,16 @@ public class WebApp extends Application {
 			HttpServer.removeServlet(WebappServletHolder.INSTANCE);
 		} catch (HttpServerException e) {
 			logger.error("Error removing servlet", e);
+		}
+	}
+	
+	@Subscribe
+	public void handleHttpEvent(HttpEvent e) {
+		// TODO maybe handle GUI events here
+		try {
+			e.reply("ciao " + e.getUser().getUsername() + " - " + e.getValue());
+		} catch (Exception e1) {
+			e1.printStackTrace();
 		}
 	}
 
